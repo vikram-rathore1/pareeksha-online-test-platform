707b6acd420cba649dc3e3d7d3d59d14ce377b89
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2018-07-03 14:58:10 +0530

    Created users app in django and custom user model

---
+++ pareeksha/users/models.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, password, is_superuser, **extra_fields):
+        if not email:
+            raise ValueError('Email cannot be blank')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email, is_superuser=is_superuser, **extra_fields)
+        user.set_password(password)
+        user.save(self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, **extra_fields)
+
+
+class User(AbstractBaseUser):
+
+    TEACHER = 1
+    STUDENT = 2
+
+    ROLE_CHOICES = (
+        (TEACHER, 'Teacher'),
+        (STUDENT, 'Student'),
+    )
+
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    role = models.IntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+    standard = models.ForeignKey('online_tests.Standard', related_name="students",
+                                 on_delete=models.SET_NULL, null=True, blank=True)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    @property
+    def is_staff(self):
+        return self.is_superuser
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_full_name(self):
+        return self.first_name
+
+    def is_teacher(self):
+        return self.role == self.TEACHER
+
+    def is_student(self):
+        return self.role == self.STUDENT
+
+    def is_test_owner(self, test_id):
+        return True if test_id and self.online_tests.filter(id=test_id).count() else False
