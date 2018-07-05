0b4cc6892785eac0bd2e40720805702b341f5784
Vikram Singh Rathore <rathore.vikram624@gmail.com>
2018-07-05 18:17:50 +0530

    Added register API

--- pareeksha/users/serializers.py
+++ pareeksha/users/serializers.py
@@ -2,6 +2,21 @@ from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import User
 
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'password', 'first_name', 'last_name', 'role',)
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'role': {'required': True, 'allow_null': False},
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
--- pareeksha/users/urls.py
+++ pareeksha/users/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from .views import LoginAPI
+from .views import LoginAPI, RegistrationAPI
 
 urlpatterns = [
     url("^login/", LoginAPI.as_view()),
+    url("^register/", RegistrationAPI.as_view()),
 ]
